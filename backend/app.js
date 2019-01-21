e00a78f5cc91fa2a5378783672b5fd81dcb33cda
Frank <fgunsch.webdev@gmail.com>
2019-01-21 18:02:33 -0500

    Removed some default boilerplate, added helmet, added unrefined routes

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +29,6 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET boards page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FMOTDB' });
+  res.render('index');
 });
 
 module.exports = router;
