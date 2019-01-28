7fa85587b72aea842adbb29970d65998cfd21f63
Frank <fgunsch.webdev@gmail.com>
2019-01-27 23:03:21 -0500

    Added fakerjs

---
+++ backend/mockData/populate.js
@@ -0,0 +1,6 @@
+//Use this to populate database with information
+var faker = require('faker');
+
+var randomName = faker.name.findName(); // Rowan Nikolaus
+var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+var randomCard = faker.helpers.createCard(); // random contact card containing many properties
