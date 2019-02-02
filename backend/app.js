55fd242ed73495c3c85148e747abcc3b58c40520
Frank <fgunsch.webdev@gmail.com>
2019-02-01 23:24:06 -0500

    Added sentry error tracking for dev

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,35 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 var helmet = require('helmet');
+const Sentry = require('@sentry/node');
 
 var indexRouter = require('./routes/index');
 var boardsRouter = require('./routes/boards');
 
 
 var app = express();
+
+Sentry.init({ dsn: 'https://42de266aafcb47708d693e26a2aec275@sentry.io/1385379' });
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+
+// Sentry test case
+// app.get('/', function mainHandler(req, res) {
+//   throw new Error('Broke!');
+// });
+
+// The error handler must be before any other error middleware
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + '\n');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
